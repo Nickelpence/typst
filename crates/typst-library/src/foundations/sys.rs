5d664bfbf8f0072b6f6cb426268146d34ea100c5
Nick Mazzu <mazzun@usi.ch>
2025-10-07 14:21:12 +0200

    janky sys.subprocess function

--- crates/typst-library/src/foundations/sys.rs
+++ crates/typst-library/src/foundations/sys.rs
@@ -1,6 +1,8 @@
 //! System-related things.
 
-use crate::foundations::{Dict, Module, Scope, Version};
+use std::io::Write;
+use std::process::{Command, Stdio};
+use crate::foundations::{SourceResult, Dict, Module, Scope, Version, Value, Args, func};
 
 /// A module with system-related things.
 pub fn module(inputs: Dict) -> Module {
@@ -13,6 +15,38 @@ pub fn module(inputs: Dict) -> Module {
             env!("CARGO_PKG_VERSION_PATCH").parse::<u32>().unwrap(),
         ]),
     );
+		scope.define_func::<subprocess>();
     scope.define("inputs", inputs);
     Module::new("sys", scope)
+}
+
+#[func(title = "subprocess")]
+pub fn subprocess(args: &mut Args) -> SourceResult<Value> {
+		let cmd = args.expect::<String>("command")?;
+		let stdin_data: Option<String> = args.named("stdin")?;
+		
+		let mut command = Command::new("sh");
+    command.arg("-c").arg(&cmd);
+		
+		command.stdin(Stdio::piped());
+		command.stdout(Stdio::piped());
+
+		let mut child = command
+        .spawn()
+        .map_err(|e| format!("Failed to start subprocess: {e}")).unwrap();
+
+    if let Some(input) = stdin_data {
+        if let Some(mut stdin) = child.stdin.take() {
+            stdin
+                .write_all(input.as_bytes())
+                .map_err(|e| format!("Failed to write to stdin: {e}")).unwrap();
+        }
+    }
+		
+		let output = child
+        .wait_with_output()
+        .map_err(|e| format!("Failed to read subprocess output: {e}")).unwrap();
+		
+		let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+		Ok(Value::Str(stdout.into()))
 }
